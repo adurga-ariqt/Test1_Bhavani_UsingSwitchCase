152f22fcc24656329715180867dcfcf031d9c2b7
adurga-ariqt <adurga@ariqt.com>
2022-11-05 12:11:01 +0530

    Add project files.

---
+++ Test1_Bhavani_UsingSwitchCase/Colors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_Bhavani_UsingSwitchCase
+{
+    public class Colors
+    {
+        public void ColorsMethod()
+        {
+            String letter;
+            Console.WriteLine("Enter Letter from the VIBGYOR:");
+            letter = Console.ReadLine();
+            switch (letter)
+            { case "v":
+                    Console.WriteLine("Violet");
+                    break;
+               case "I":
+                    Console.WriteLine("Indigo color");
+                    break;
+                case "B":
+                    Console.WriteLine("Blue Color");
+                    break;
+                case "G":
+                    Console.WriteLine("Green Color");
+                    break;
+                case "Y":
+                    Console.WriteLine("Yellow colour");
+                    break;
+                case "O":
+                    Console.WriteLine("Orange Colour");
+                    break;
+                case "R":
+                    Console.WriteLine("Red Colour");
+                    break;
+                default:
+                    Console.WriteLine("Not a colour from VIBGYOR");
+                    break;
+
+            }
+
+
+        }    
+    }
+}
---
+++ Test1_Bhavani_UsingSwitchCase/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test1_Bhavani_UsingSwitchCase;
+
+Console.WriteLine("Hello, World!");
+Colors objColors = new Colors();
+objColors.ColorsMethod();
